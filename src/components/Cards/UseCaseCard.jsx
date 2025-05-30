dc341fe0c4259ee108690123cba3a0b0177a1331
samuelogn <onyedikasgn@gmail.com>
2025-05-31 00:05:03 +0100

    Fixed image sizing and changed roles in waitlist form

--- src/components/Cards/UseCaseCard.jsx
+++ src/components/Cards/UseCaseCard.jsx
@@ -4,7 +4,7 @@ const UseCaseCard = ({h6Content, p2Content, image}) => {
   return (
     <div className="flex flex-col gap-7 lg:gap-9 w-full lg:w-[30rem] shrink-0">
       <div className="rounded-md md:rounded-lg bg-grey3 w-full aspect-[4/5]">
-        <img src={image} alt="Use case image"/>
+        <img src={image} alt="Use case image" className="w-full h-full object-cover"/>
       </div>
       <div className="flex flex-col gap-2 lg:gap-4">
         <H6 content={h6Content} />
